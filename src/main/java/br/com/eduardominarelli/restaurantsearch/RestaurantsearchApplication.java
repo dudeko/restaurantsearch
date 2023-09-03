e0043f0238639078eab08ef3f5951dfdda96f45f
Eduardo Minarelli <dudeko@gmail.com>
2023-09-03 12:48:19 -0300

    Starting project with Spring Initializr

---
+++ src/main/java/br/com/eduardominarelli/restaurantsearch/RestaurantsearchApplication.java
@@ -0,0 +1,13 @@
+package br.com.eduardominarelli.restaurantsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsearchApplication.class, args);
+	}
+
+}
